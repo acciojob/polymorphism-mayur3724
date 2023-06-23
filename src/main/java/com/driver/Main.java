590072607ea2d2ec78ffa139c84bedd294bc4a35
mayur@3724 <mayurpagare992@gmail.com>
2023-06-24 01:14:36 +0530

    ok

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
-public class Main {
+import org.w3c.dom.ls.LSOutput;
 
+public  class Main {
+public static void main(String[] args){
+    System.out.println("Hello world");
+    }
 }
